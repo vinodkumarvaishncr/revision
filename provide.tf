fac6317164a76f9a8fadb4aff1d546f9bfbba41e
unknown <vinodkumarvaishncr@gmail.com>
2025-07-01 20:27:29 +0530

    add main and prove

---
+++ provide.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.25.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "247d5ce7-c0a1-4b83-8d9c-30b55c958e6c"
+}
